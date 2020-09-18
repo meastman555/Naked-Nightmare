b644957eb416d899557678be41ff072424d11bf6
jojohorak <jehorak@utexas.edu>
2020-09-18 15:05:32 -0500

    stuf

---
+++ UTGJ2020Unity/Assets/Scripts/PlayerSorting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSorting : MonoBehaviour
+{
+
+    private Renderer myRend;
+    [SerializeField]
+    private int sortingOrderBase = 500;
+    [SerializeField]
+    private int offset = 0;
+    [SerializeField]
+    private bool runOnlyOnce = false;
+
+    private void Awake()
+    {
+        myRend = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        myRend.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+        if(runOnlyOnce)
+        {
+            Destroy(this);
+        }
+    }
+
+}
