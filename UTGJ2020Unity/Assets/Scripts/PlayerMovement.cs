35613a5bf74b17aa3937f73554ebcb0e1329a1a2
jojohorak <jehorak@utexas.edu>
2020-09-18 17:09:54 -0500

    Art Prefabs

--- UTGJ2020Unity/Assets/Scripts/PlayerMovement.cs
+++ UTGJ2020Unity/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,17 @@ public class PlayerMovement : MonoBehaviour
 
     //if not initialized in editor default value is 10
     [SerializeField] float speed = 10;
+    [SerializeField] Sprite[] sprites;
+
+    private int spriteIndex;
+    private SpriteRenderer sr;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,18 +31,23 @@ public class PlayerMovement : MonoBehaviour
         //moving in all 4 directions
         if(Input.GetKey(KeyCode.W)){
             rb.velocity = Vector2.up * speed;
+            sr.sprite = sprites[2];
         }
         else if(Input.GetKey(KeyCode.S)){
             rb.velocity = Vector2.down * speed;
+            sr.sprite = sprites[0];
         }
         else if(Input.GetKey(KeyCode.A)){
             rb.velocity = Vector2.left * speed;
+            sr.sprite = sprites[3];
         }
         else if(Input.GetKey(KeyCode.D)){
             rb.velocity = Vector2.right * speed;
+            sr.sprite = sprites[1];
         }
         //stationary
-        else{
+        else
+        {
             rb.velocity = Vector2.zero;
         }
     }
