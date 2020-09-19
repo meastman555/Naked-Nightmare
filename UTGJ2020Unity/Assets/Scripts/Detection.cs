04a8bb6973afcd07905706ab075bc60659168495
Mason Eastman <masoneastman555@gmail.com>
2020-09-18 21:36:51 -0500

    working detection and collision

--- UTGJ2020Unity/Assets/Scripts/Detection.cs
+++ UTGJ2020Unity/Assets/Scripts/Detection.cs
@@ -9,6 +9,8 @@ public class Detection : MonoBehaviour
     [SerializeField] float fov;
     [SerializeField] float maxDistance;
 
+    public LayerMask masks;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,27 +21,38 @@ public class Detection : MonoBehaviour
     void Update()
     {
         //Debug to test with rotation
-        //if(Input.GetKey(KeyCode.Space)){
-        //    transform.Rotate(0, 0, 100 * Time.deltaTime);
-        //}
+        if(Input.GetKey(KeyCode.Space)){
+            transform.Rotate(0, 0, 100 * Time.deltaTime);
+        }
     }
 
     //adapted from Unity docs on Vector3.angle and forum on basic cone FOV
     void FixedUpdate(){
         //calculates the direction vector, distance between two objects (magnitude of distance), and angle between axis and direction
-        Vector3 directionVector = transform.position - player.transform.position;
+        Vector3 directionVector =  player.transform.position - transform.position;
         float distance = directionVector.magnitude;
         //transform.right could be changed depending on starting rotation of enemies
-        float angle = Vector3.Angle(directionVector, transform.right);
+        float angle = Vector3.Angle(directionVector, -transform.right);
         //conditions for detection
         if(angle < (fov / 2f) && distance <= maxDistance){
-            Debug.DrawLine(transform.position, player.transform.position, Color.red);
+            //player is within vision cone, but have to check if there is anything in the way
+            RaycastHit2D hit = Physics2D.Linecast(transform.position, player.transform.position, masks);
+            if(hit.collider != null && hit.collider.tag == "Player"){
+                Debug.Log("Hit the player!");
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+            } 
             //detection has occurred, call movement script to follow
             //or game just fails?
+            else{
+                Debug.Log("Hit object in front of player! (NULL)");
+                Debug.DrawLine(transform.position, player.transform.position, Color.blue);
+
+            }
         }
         //safety (so do nothing?)
         else{
             Debug.DrawLine(transform.position, player.transform.position, Color.green);
+            Debug.Log("Can't hit player");
         }
     }
 }
