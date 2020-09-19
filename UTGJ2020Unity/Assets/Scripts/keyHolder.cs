9bbe6cb033ec16602172a64aaa1eb47bd94d4ec9
jojohorak <jehorak@utexas.edu>
2020-09-19 16:00:49 -0500

    keyUI

--- UTGJ2020Unity/Assets/Scripts/keyHolder.cs
+++ UTGJ2020Unity/Assets/Scripts/keyHolder.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class keyHolder : MonoBehaviour
 {
 
     private List<Key.KeyType> keyList;
+
+    [SerializeField]
+    private GameObject keyRed, keyGreen, keyBlue, keyPurple, keyGolden;
 
     private void Awake()
     {
@@ -34,6 +40,8 @@ public class keyHolder : MonoBehaviour
         {
             AddKey(key.GetKeyType());
             Destroy(key.gameObject);
+            Key.KeyType keyType = key.GetKeyType();
+            ShowUI(keyType);
         }
 
         keyDoor keyDoor = collision.GetComponent<keyDoor>();
@@ -42,8 +50,67 @@ public class keyHolder : MonoBehaviour
             if(ContainsKey(keyDoor.GetKeyType()))
             {
                 keyDoor.OpenDoor();
+                HideUI(keyDoor.GetKeyType());
                 RemoveKey(keyDoor.GetKeyType());
             }
+        }
+    }
+
+    void ShowUI(Key.KeyType keyType)
+    {
+        if(keyType == Key.KeyType.Red)
+        {
+            keyRed.SetActive(true);
+            print("show " + keyType);
+        }
+        else if(keyType == Key.KeyType.Green)
+        {
+            keyGreen.SetActive(true);
+            print("show " + keyType);
+        }
+        else if(keyType == Key.KeyType.Blue)
+        {
+            keyBlue.SetActive(true);
+            print("show " + keyType);
+        }
+        else if(keyType == Key.KeyType.Purple)
+        {
+            keyPurple.SetActive(true);
+            print("show " + keyType);
+        }
+        else if(keyType == Key.KeyType.Golden)
+        {
+            keyGolden.SetActive(true);
+            print("show " + keyType);
+        }
+    }
+
+    void HideUI(Key.KeyType keyType)
+    {
+        if (keyType == Key.KeyType.Red)
+        {
+            keyRed.SetActive(false);
+            print("show " + keyType);
+        }
+        else if (keyType == Key.KeyType.Green)
+        {
+            keyGreen.SetActive(false);
+            print("show " + keyType);
+        }
+        else if (keyType == Key.KeyType.Blue)
+        {
+            keyBlue.SetActive(false);
+            print("show " + keyType);
+        }
+        else if (keyType == Key.KeyType.Purple)
+        {
+            keyPurple.SetActive(false);
+            print("show " + keyType);
+        }
+        else if (keyType == Key.KeyType.Golden)
+        {
+            keyGolden.SetActive(false);
+            print("show " + keyType);
         }
     }
 
