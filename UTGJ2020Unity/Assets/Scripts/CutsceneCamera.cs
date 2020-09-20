b8d2f0c303b3dcdb22227dbb03a7b027e8a450e7
trendywalnut <connorb23@utexas.edu>
2020-09-20 01:39:41 -0500

    updated everything

---
+++ UTGJ2020Unity/Assets/Scripts/CutsceneCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneCamera : MonoBehaviour
+{
+
+    [SerializeField] Camera cam_1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam_1.transform.position = new Vector3(-2.6f, 2f, -10f);
+        cam_1.GetComponent<Animator>().Play("Camera");
+        Invoke("LoadGame", 12);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadGame(){
+        Debug.Log("Loading game scene");
+        SceneManager.LoadScene(2);
+    }
+}
